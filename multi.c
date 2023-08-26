bd02be7a8ee14daff8385e6723965ab5cd3801ea
prateek <gadpayleprateek@gmail.com>
2023-08-26 17:20:22 +0530

    multiply

---
+++ multi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int multiply(int a, int b){
+    return (a*b);
+}
+
+
+int main(){
+
+ int m = 20;
+ int n = 10;
+
+ int result = sum(m,n);
+ printf("SUM =  %d",result);
+ return 0;
+
+
+
+}
